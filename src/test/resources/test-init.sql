4178dd4020f37c72a37e1539f0222e3b05ba502f
Nikita Vertelko <vernik03@gmail.com>
2023-06-07 03:03:21 +0300

    database changes

--- src/test/resources/test-init.sql
+++ src/test/resources/test-init.sql
@@ -1,26 +1,37 @@
-INSERT INTO users (id, name, position, surname) VALUES (1, 'Viktor', 'NAVIGATOR', 'Muzyka');
-INSERT INTO users (id, name, position, surname) VALUES (2, 'Petro', 'PILOT', 'Donchuk-Dontsov');
-INSERT INTO users (id, name, position, surname) VALUES (3, 'Stepan', 'OPERATOR', 'Ilchuk');
-INSERT INTO users (id, name, position, surname) VALUES (4, 'Roman', 'PILOT', 'Prokopchuk');
-ALTER TABLE users ALTER COLUMN id RESTART WITH 5;
+INSERT INTO users (id, name, is_admin, login, password) VALUES (1, 'Peter Parker', false, 'peter', 'PeTeR');
+INSERT INTO users (id, name, is_admin, login, password) VALUES (2, 'Karl Black', false, 'mr_black', '123456789');
+INSERT INTO users (id, name, is_admin, login, password) VALUES (3, 'Robert White', true, 'robert', 'admin');
+ALTER TABLE users ALTER COLUMN id RESTART WITH 4;
 
-INSERT INTO credit_cards (id, departure_time, departure_from, arrival_time, destination)
-VALUES (1, '2023-05-23 23:53:00' , 'Kyiv', '2023-05-24 16:09:00', 'Krakow');
+INSERT INTO bank_accounts (id, card_number, balance, is_blocked)
+VALUES (1, '5748567890123456', 1000, false);
+INSERT INTO bank_accounts (id, card_number, balance, is_blocked)
+VALUES (2, '5563767890167847', 2000, false);
+INSERT INTO bank_accounts (id, card_number, balance, is_blocked)
+VALUES (3, '3420127893456847', 3000, false);
+INSERT INTO bank_accounts (id, card_number, balance, is_blocked)
+VALUES (4, '7983767890167847', 4000, false);
+INSERT INTO bank_accounts (id, card_number, balance, is_blocked)
+VALUES (5, '6463700000167847', 1500, false);
+INSERT INTO bank_accounts (id, card_number,  balance, is_blocked)
+VALUES (6, '2463427893467847', 300, false);
+INSERT INTO bank_accounts (id, card_number, balance, is_blocked)
+VALUES (7, '5563767890167847', 6700, false);
 
-INSERT INTO credit_cards (id, departure_time, departure_from, arrival_time, destination)
-VALUES (2, '2023-05-24 06:41:00' , 'Frankurt', '2023-05-24 20:59:00', 'New York');
+ALTER TABLE credit_cards ALTER COLUMN id RESTART WITH 8;
 
-INSERT INTO credit_cards (id, departure_time, departure_from, arrival_time, destination)
-VALUES (3, '2023-04-12 15:12:00' , 'Ankara', '2023-04-13 00:49:00', 'Berlin');
+INSERT INTO credit_cards (fk_user_id, fk_bank_account_id)
+VALUES (1, 1);
+INSERT INTO credit_cards (fk_user_id, fk_bank_account_id)
+VALUES (2, 2);
+INSERT INTO credit_cards (fk_user_id, fk_bank_account_id)
+VALUES (3, 3);
+INSERT INTO credit_cards (fk_user_id, fk_bank_account_id)
+VALUES (3, 4);
+INSERT INTO credit_cards (fk_user_id, fk_bank_account_id)
+VALUES (2, 5);
+INSERT INTO credit_cards (fk_user_id, fk_bank_account_id)
+VALUES (1, 6);
+INSERT INTO credit_cards (fk_user_id, fk_bank_account_id)
+VALUES (2, 7);
 
-ALTER TABLE credit_cards ALTER COLUMN id RESTART WITH 4;
-
-INSERT INTO bank_accounts (fk_crew_member_id, fk_flight_id) VALUES (1, 1);
-INSERT INTO bank_accounts (fk_crew_member_id, fk_flight_id) VALUES (3, 1);
-INSERT INTO bank_accounts (fk_crew_member_id, fk_flight_id) VALUES (4, 1);
-
-INSERT INTO bank_accounts (fk_crew_member_id, fk_flight_id) VALUES (2, 2);
-INSERT INTO bank_accounts (fk_crew_member_id, fk_flight_id) VALUES (3, 2);
-
-INSERT INTO bank_accounts (fk_crew_member_id, fk_flight_id) VALUES (1, 3);
-INSERT INTO bank_accounts (fk_crew_member_id, fk_flight_id) VALUES (4, 3);
